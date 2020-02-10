4da5ef7c6cd2d5d8cc5919e821695420a8ac27da
Sanketmundada <mundadasanket2000@gmail.com>
2020-02-11 00:48:24 +0530

    Again Update :)

--- src/pages/LeaderBoard/leaderboard.js
+++ src/pages/LeaderBoard/leaderboard.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "../../mainComponents/Navbar";
-import Footer from "./components/footer";
+import Footer from "../../mainComponents/footer";
 import FinalBoard from "./components/finalBoard";
-import Title from "./title"
+import Title from "./title";
 
 class Leaderboard extends Component {
   state = {};
@@ -14,9 +14,9 @@ class Leaderboard extends Component {
         <div className="row" style={{ height: "93vh" }}>
           <Navbar />
           <div className="col-sm-10">
-          <div className="row" style={{ height: "7vh" }}>
-            <Title title="Leaderboard"/>
-           </div>
+            <div className="row" style={{ height: "7vh" }}>
+              <Title title="Leaderboard" />
+            </div>
             <div className="row" style={{ height: "86vh" }}>
               <FinalBoard />
             </div>
