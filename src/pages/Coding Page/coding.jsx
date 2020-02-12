fe01f20ca3228ea6df63f47e46b0461b7a6a6ce4
anishkoulgi <anish.koulgi@gmail.com>
2020-02-12 19:22:02 +0530

    Question file added

--- src/pages/Coding Page/coding.jsx
+++ src/pages/Coding Page/coding.jsx
@@ -7,14 +7,15 @@ import CodingPage from "./codingPage";
 
 class Coding extends Component {
   state = {
-    class1: "black",
+    class1: "highlight",
     class2: "black",
     class3: "black",
     class4: "black",
     class5: "black",
     class6: "black",
     class7: "black",
-    class8: "black"
+    class8: "black",
+    questionNumber: 1
   };
 
   handlePassedValue(val) {
@@ -27,7 +28,8 @@ class Coding extends Component {
         class5: "black",
         class6: "black",
         class7: "black",
-        class8: "black"
+        class8: "black",
+        questionNumber: 1
       });
     } else if (val === 2) {
       this.setState({
@@ -38,7 +40,8 @@ class Coding extends Component {
         class5: "black",
         class6: "black",
         class7: "black",
-        class8: "black"
+        class8: "black",
+        questionNumber: 2
       });
     } else if (val === 3) {
       this.setState({
@@ -49,7 +52,8 @@ class Coding extends Component {
         class5: "black",
         class6: "black",
         class7: "black",
-        class8: "black"
+        class8: "black",
+        questionNumber: 3
       });
     } else if (val === 4) {
       this.setState({
@@ -60,7 +64,8 @@ class Coding extends Component {
         class5: "black",
         class6: "black",
         class7: "black",
-        class8: "black"
+        class8: "black",
+        questionNumber: 4
       });
     } else if (val === 5) {
       this.setState({
@@ -71,7 +76,8 @@ class Coding extends Component {
         class5: "highlight",
         class6: "black",
         class7: "black",
-        class8: "black"
+        class8: "black",
+        questionNumber: 5
       });
     } else if (val === 6) {
       this.setState({
@@ -82,7 +88,8 @@ class Coding extends Component {
         class5: "black",
         class6: "highlight",
         class7: "black",
-        class8: "black"
+        class8: "black",
+        questionNumber: 6
       });
     } else if (val === 7) {
       this.setState({
@@ -93,7 +100,8 @@ class Coding extends Component {
         class5: "black",
         class6: "black",
         class7: "highlight",
-        class8: "black"
+        class8: "black",
+        questionNumber: 7
       });
     } else if (val === 8) {
       this.setState({
@@ -104,7 +112,8 @@ class Coding extends Component {
         class5: "black",
         class6: "black",
         class7: "black",
-        class8: "highlight"
+        class8: "highlight",
+        questionNumber: 8
       });
     }
   }
@@ -112,35 +121,99 @@ class Coding extends Component {
   componentDidMount() {
     if (this.props.location.param2 === 1) {
       this.setState({
-        class1: "highlight"
+        class1: "highlight",
+        class2: "black",
+        class3: "black",
+        class4: "black",
+        class5: "black",
+        class6: "black",
+        class7: "black",
+        class8: "black",
+        questionNumber: 1
       });
     } else if (this.props.location.param2 === 2) {
       this.setState({
-        class2: "highlight"
+        class1: "black",
+        class2: "highlight",
+        class3: "black",
+        class4: "black",
+        class5: "black",
+        class6: "black",
+        class7: "black",
+        class8: "black",
+        questionNumber: 2
       });
     } else if (this.props.location.param2 === 3) {
       this.setState({
-        class3: "highlight"
+        class1: "black",
+        class2: "black",
+        class3: "highlight",
+        class4: "black",
+        class5: "black",
+        class6: "black",
+        class7: "black",
+        class8: "black",
+        questionNumber: 3
       });
     } else if (this.props.location.param2 === 4) {
       this.setState({
-        class4: "highlight"
+        class1: "black",
+        class2: "black",
+        class3: "black",
+        class4: "highlight",
+        class5: "black",
+        class6: "black",
+        class7: "black",
+        class8: "black",
+        questionNumber: 4
       });
     } else if (this.props.location.param2 === 5) {
       this.setState({
-        class5: "highlight"
+        class1: "black",
+        class2: "black",
+        class3: "black",
+        class4: "black",
+        class5: "highlight",
+        class6: "black",
+        class7: "black",
+        class8: "black",
+        questionNumber: 5
       });
     } else if (this.props.location.param2 === 6) {
       this.setState({
-        class6: "highlight"
+        class1: "black",
+        class2: "black",
+        class3: "black",
+        class4: "black",
+        class5: "black",
+        class6: "highlight",
+        class7: "black",
+        class8: "black",
+        questionNumber: 6
       });
     } else if (this.props.location.param2 === 7) {
       this.setState({
-        class7: "highlight"
+        class1: "black",
+        class2: "black",
+        class3: "black",
+        class4: "black",
+        class5: "black",
+        class6: "black",
+        class7: "highlight",
+        class8: "black",
+        questionNumber: 7
       });
     } else if (this.props.location.param2 === 8) {
       this.setState({
-        class8: "highlight"
+        class1: "black",
+        class2: "black",
+        class3: "black",
+        class4: "black",
+        class5: "black",
+        class6: "black",
+        class7: "black",
+        class8: "highlight",
+        questionNumber: 8
       });
     }
   }
@@ -149,18 +222,18 @@ class Coding extends Component {
     //console.log(this.props.location.param2);
 
     return (
-              <CodingPage
-                handlePassedValue={this.handlePassedValue.bind(this)}
-                class1={this.state.class1}
-                class2={this.state.class2}
-                class3={this.state.class3}
-                class4={this.state.class4}
-                class5={this.state.class5}
-                class6={this.state.class6}
-                class7={this.state.class7}
-                class8={this.state.class8}
-              />
-  
+      <CodingPage
+        handlePassedValue={this.handlePassedValue.bind(this)}
+        class1={this.state.class1}
+        class2={this.state.class2}
+        class3={this.state.class3}
+        class4={this.state.class4}
+        class5={this.state.class5}
+        class6={this.state.class6}
+        class7={this.state.class7}
+        class8={this.state.class8}
+        questionNumber={this.state.questionNumber}
+      />
     );
   }
 }
--- src/pages/Coding Page/codingPage.jsx
+++ src/pages/Coding Page/codingPage.jsx
@@ -10,6 +10,7 @@ import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/clike/clike";
 import "codemirror/addon/edit/closebrackets.js";
+import Question from "../../mainComponents/Questions";
 
 class CodingPage extends Component {
   constructor(props) {
@@ -17,12 +18,10 @@ class CodingPage extends Component {
 
     this.state = {
       redirect: false,
-      renderConsole : false
+      renderConsole: false
     };
-
-   
   }
-   
+
   passValue(val) {
     this.props.handlePassedValue(val);
   }
@@ -32,13 +31,12 @@ class CodingPage extends Component {
       redirect: true
     });
   };
-  
-  handleConsole=() => {
+
+  handleConsole = () => {
     this.setState({
       renderConsole: true
     });
- 
-  }
+  };
 
   render() {
     if (this.state.redirect === true) {
@@ -88,129 +86,11 @@ class CodingPage extends Component {
                 overflow: "auto"
               }}
             >
-              Given a 2D array of digits or grid, try to find the occurrence of
-              a given 2D pattern of digits. For example, consider the following
-              grid:
-              <br />
-              1234567890 <br />
-              0987654321 <br />
-              1111111111 <br />
-              1111111111 <br />
-              2222222222 <br />
-              Assume we need to look for the following 2D pattern array:
-              <br />
-              876543
-              <br />
-              111111 <br />
-              111111
-              <br />
-              The 2D pattern begins at the second row and the third column of
-              the grid. The pattern is said to be present in the grid.
-              <br />
-              Function Description
-              <br />
-              Complete th gridSearch function in the editor below. It should
-              return YES if the pattern exists in the grid, or NO otherwise.
-              <br />
-              gridSearch has the following parameter(s):
-              <br />
-              G: the grid to search, an array of strings
-              <br />
-              P: the pattern to search for, an array of strings
-              <br />
-              Input Format
-              <br />
-              The first line contains an integer , the number of test cases.
-              <br />
-              Each of the test cases is represented as follows:
-              <br />
-              The first line contains two space-separated integers and ,
-              indicating the number of rows and columns in the grid . This is
-              followed by lines, each with a string of digits representing the
-              grid . The following line contains two space-separated integers,
-              and , indicating the number of rows and columns in the pattern
-              grid . This is followed by lines, each with a string of digits
-              representing the Constraints
-              <br />
-              Output Format
-              <br />
-              Display YES or NO, depending on whether is present in . Sample
-              Input
-              <br />
-              2<br />
-              10 10
-              <br />
-              7283455864
-              <br />
-              6731158619
-              <br />
-              8988242643
-              <br />
-              3830589324
-              <br />
-              2229505813
-              <br />
-              5633845374
-              <br />
-              6473530293
-              <br />
-              7053106601
-              <br />
-              0834282956
-              <br />
-              4607924137
-              <br />
-              3 4<br />
-              9505
-              <br />
-              3845
-              <br />
-              3530
-              <br />
-              15 15
-              <br />
-              400453592126560
-              <br />
-              114213133098692
-              <br />
-              474386082879648
-              <br />
-              522356951189169
-              <br />
-              887109450487496
-              <br />
-              252802633388782
-              <br />
-              502771484966748
-              <br />
-              075975207693780
-              <br />
-              511799789562806
-              <br />
-              404007454272504
-              <br />
-              549043809916080
-              <br />
-              962410809534811
-              <br />
-              445893523733475
-              <br />
-              768705303214174
-              <br />
-              650629270887160
-              <br />
-              2 2<br />
-              99
-              <br />
-              99
-              <br />
-              Sample Output
-              <br />
-              YES NO Explanation
-              <br />
-              The first test in the input file is:
-              <br />
-              10 10 7283455864
+              {Question.map((question, key) => {
+                if (this.props.questionNumber === question.key) {
+                  return question.questionName;
+                }
+              })}
             </div>
             <div
               style={{
@@ -221,8 +101,7 @@ class CodingPage extends Component {
                 display: "block",
                 justifyContent: "center",
                 alignContent: "center",
-                borderRadius: "5px",
-                
+                borderRadius: "5px"
               }}
             >
               <span style={{ display: "flex" }}>
@@ -280,13 +159,13 @@ class CodingPage extends Component {
                     width: "10vw",
                     marginLeft: "1vw"
                   }}
-                  onClick = {this.handleConsole}
+                  onClick={this.handleConsole}
                 >
                   Run
                 </button>
               </span>
             </div>
-            <Console  render={this.state.renderConsole}/>
+            <Console render={this.state.renderConsole} />
           </div>
         </div>
       </div>
